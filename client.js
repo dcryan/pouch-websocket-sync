b317490b91cfdb8944993f019424174083d01426
Daniel Ryan <daniel.c.ryan@icloud.com>
2017-01-22 15:52:54 -0700

    Removed function hoisting.

--- client.js
+++ client.js
@@ -1,20 +1,22 @@
 var Websocket = require('websocket-stream');
 var PouchSync = require('pouch-stream-multi-sync');
 
-module.exports = createClient;
-
 function createClient() {
-  var client = PouchSync.createClient(function connect(address) {
-    var ws = Websocket(address);
-    ws.on('error', onError);
-    return ws;
-  });
-  return client;
-
   /* istanbul ignore next */
   function onError(err) {
     if (err.message !== 'write after end') {
       client.emit('error', err);
     }
   }
+  
+  var client = PouchSync.createClient(function connect(address) {
+    var ws = Websocket(address);
+    ws.on('error', onError);
+    return ws;
+  });
+  
+  return client;
+
 }
+
+module.exports = createClient;
